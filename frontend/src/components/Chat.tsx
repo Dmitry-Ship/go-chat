b74c3d48db09c6131e7474f9bdf4a874850e3bc4
Dmitry Shipunov <vooxil@gmail.com>
2021-08-15 20:20:36 +0300

    visual fixes

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -23,6 +23,16 @@ const Chat = () => {
       }));
       appendLog(messages);
     });
+
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
   }, []);
 
   const handleSubmit = (e: React.MouseEvent<HTMLElement>) => {
